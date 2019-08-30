e70dabca0635744dcf6e9becbb4a9570d1eddb80
yuki0n0 <10773910+yuki0n0@users.noreply.github.com>
2019-08-31 06:23:20 +0900

    js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const request = require('request');
 
+const URL = process.env.INPUT_URL;
+
+request.post({
+    uri: URL,
+    headers: { "Content-type": "application/json" },
+    json: {
+        "text": process.env.INPUT_TEXT
+    }
+});
