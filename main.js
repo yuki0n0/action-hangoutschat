9399c539db76601353fa5999386587d7d0963927
yuki0n0 <10773910+yuki0n0@users.noreply.github.com>
2019-08-31 07:53:45 +0900

    変数増やした

--- main.js
+++ main.js
@@ -1,11 +1,69 @@
 const request = require('request');
-
 const URL = process.env.INPUT_URL;
+
+const text = process.env.INPUT_TEXT;
+const headerTitle = process.env.INPUT_HEADER_TITLE;
+const headerSubtitle = process.env.INPUT_HEADER_SUBTITLE;
+const headerImage = process.env.INPUT_HEADER_IMAGE;
+
+const buttonFirst = process.env.INPUT_BUTTON_FIRST;
+const buttonSecond = process.env.INPUT_BUTTON_SECOND;
+
+var card = {};
+
+if (headerTitle || headerSubtitle || headerImage) {
+    card["header"] = {
+        "title": headerTitle,
+        "subtitle": headerSubtitle,
+        "imageUrl": headerImage,
+    }
+}
+
+const buttonFirstMatch = buttonFirst.match(/^\[(.+)\]\((.*)\)$/);
+if (buttonFirstMatch) {
+    card["sections"] = [
+        {
+            "widgets": [
+                {
+                    "buttons": [
+                        {
+                            "textButton": {
+                                "text": buttonFirstMatch[1],
+                                "onClick": {
+                                    "openLink": {
+                                        "url": buttonFirstMatch[2]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+const buttonSecondMatch = buttonSecond.match(/^\[(.+)\]\((.*)\)$/);
+if (buttonSecondMatch) {
+    card["sections"][0]["widgets"][0]["buttons"][1] = {
+        "textButton": {
+            "text": buttonSecondMatch[1],
+            "onClick": {
+                "openLink": {
+                    "url": buttonSecondMatch[2]
+                }
+            }
+        }
+    }
+}
 
 request.post({
     uri: URL,
     headers: { "Content-type": "application/json" },
     json: {
-        "text": process.env.INPUT_TEXT
+        "text": text,
+        "cards": [card]
     }
+}, (err, res, data) => {
+    console.log(err, res, data);
 });
